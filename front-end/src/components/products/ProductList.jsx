dc2739f6f6e0c49ca21b006b4ecf85f0c625b3d2
adviro22 <ajosueramos@gmail.com>
2023-08-17 16:14:31 -0500

    Proyecto terminado V1

--- front-end/src/components/products/ProductList.jsx
+++ front-end/src/components/products/ProductList.jsx
@@ -10,6 +10,7 @@ export function ProductList({ products }) { // Cambio aquí para recibir 'produc
       <thead className="bg-red-800">
         <tr>
           <th className="border-black border-4 p-2 text-xl text-black">#</th>
+          <th className="border-black border-4 p-2 text-xl text-black"><b>CODIGO del Producto</b></th>
           <th className="border-black border-4 p-2 text-xl text-black"><b>Nombre del Producto</b></th>
           <th className="border-black border-4 p-2 text-xl text-black"><b>Precio</b></th>
           <th className="border-black border-4 p-2 text-xl text-black"><b>Stock</b></th>
@@ -20,6 +21,7 @@ export function ProductList({ products }) { // Cambio aquí para recibir 'produc
         {products.map((product, index) => (
           <tr key={product._id}>
             <td className="border-black border-4 p-2 text-xl text-black">{index + 1}</td>
+            <td className="border-black border-4 p-2 text-black">{product._id}</td>
             <td className="border-black border-4 p-2 text-black">{product.nombre}</td>
             <td className="border-black border-4 p-2 text-black">$ {product.precio}</td>
             <td className="border-black border-4 p-2 text-black">{product.stock}</td>
